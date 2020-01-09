462288016ebc1e0bf2a58e4fb4d681ede1269257
elpat7 <aguilarpat7@gmail.com>
2020-01-09 11:13:47 -0800

    updating folder structure

--- controllers/routes/client/htmlRoutes.js
+++ controllers/routes/client/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view/index.html'));
+    res.sendFile(path.join(__dirname, '../../views/index.html'));
 });
 
 module.exports = router;
