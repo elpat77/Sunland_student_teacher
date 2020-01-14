8cabf7c28bda83ff79286bbf3c674e9cb61cd6ae
SleepyJen <jennifer.ni0924@gmail.com>
2020-01-13 17:30:46 -0800

    set up sql for database

---
+++ models/config.js
@@ -0,0 +1,15 @@
+const sequelize = require('sequelize');
+require('dotenv').config();
+
+const seq = new Sequelize('school_db', 'root', process.env.PASSWORD, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+module.exports = seq;
