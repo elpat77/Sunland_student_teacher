323d6072b16fdbc2f35ac1a5a74ef6c50e7f2189
SleepyJen <jennifer.ni0924@gmail.com>
2020-01-19 22:54:25 -0800

    created an authentication

---
+++ passport-config.js
@@ -0,0 +1,25 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (user == null) {
+            return done(null, false, { message: 'No user with that email' });
+        }
+
+        try {
+            if (password.compareTo(user.password) === 0) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'incorrect password' })
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((id, done) => done(null, getUserById(id)));
+}
+
+module.exports = initialize;
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 const db = require('./models');
 
+const flash = require('express-flash');
+const session = require('express-session');
+
+const passport = require('passport');
+const initializePassport = require('./passport-config');
+initializePassport(passport, email => {
+    return users.find(user => user.email === email)
+}, id => {
+    return users.find(user => user.id === id)
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./views'));
+app.use(flash);
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const teacherRoutes = require('./controllers/api/teacherRoutes');
 app.use('/teacherRoutes', teacherRoutes);
@@ -24,6 +47,33 @@ app.use('/gradesRoutes', gradesRoutes);
 
 const htmlRoutes = require('./controllers/client/htmlRoutes');
 app.use('/', htmlRoutes);
+
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
+});
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+function checkAuthenicated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+function checkNotAuthenicated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    } else {
+        next();
+    }
+}
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
