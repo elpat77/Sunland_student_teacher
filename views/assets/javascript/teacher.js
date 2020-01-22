30865d981e3f0e310bcdf3d949a8f58c44b14e76
SleepyJen <jennifer.ni0924@gmail.com>
2020-01-22 01:38:27 -0800

    gave functionality to creating a teacher and student account

--- views/assets/javascript/teacher.js
+++ views/assets/javascript/teacher.js
@@ -1,78 +1,5 @@
 $(document).ready(function () {
 
-    //Creating Account -------------------------------------------------------------
-    $('#signUpbtn').on('click', function (e) {
-        e.preventDefault();
-
-        let email = $('#teacherEmail').val();
-        let password = $('#teacherPassword').val();
-        let firstName = $('#teacherFirstName').val();
-        let lastName = $('#teacherLastName').val();
-        let fullName = firstName + ' ' + lastName;
-
-        if (email === '') {
-            $('#emailVal').show();
-        }
-        if (password === '') {
-            $('#passwordVal').show();
-        }
-        if (firstName === '') {
-            $('#firstNameVal').show();
-        }
-        if (lastName === '') {
-            $('#lastNameVal').show();
-        }
-        if (email != '' && password != '' && firstName != '' && lastName != '') {
-            $.ajax({
-                method: 'GET',
-                url: '/emails/teacherEmails'
-            }).then(res => {
-                let teachers = new Set();
-                for (let i = 0; i < res.length; i++) {
-                    teachers.add(res[i].email);
-                }
-                if (teachers.has(email)) {
-                    login(result => {
-                        console.log(result);
-                        let teachersSet = new Set();
-                        for (let j = 0; j < result.length; j++) {
-                            teachersSet.add(result[j].email);
-                        }
-                        if (!teachersSet.has(email)) {
-                            createAccount(fullName, email, password, resultAccount => {
-                                console.log(resultAccount);
-                            });
-                            window.location.href = '/teacherlogin';
-                        } else {
-                            alert('This email has an account already');
-                        }
-
-                    });
-                } else {
-                    alert('sorry, you must be registered by the school');
-                }
-
-            });
-        }
-    });
-    //--------------------------------------------------------------------------------
-
-    //Validation ---------------------------------------------------------------------
-    $('#teacherEmail').on('click', function () {
-        $('#emailVal').hide();
-    });
-    $('#teacherPassword').on('click', function () {
-        $('#passwordVal').hide();
-    });
-    $('#teacherFirstName').on('click', function () {
-        $('#firstNameVal').hide();
-    });
-    $('#teacherLastName').on('click', function () {
-        $('#lastNameVal').hide();
-    });
-
-    //--------------------------------------------------------------------------------
-
     //Log in -------------------------------------------------------------------------
     $('#signIn').on('click', function (e) {
         e.preventDefault();
@@ -151,30 +78,5 @@ $(document).ready(function () {
         console.log('clicked');
     });
     //-------------------------------------------------------------------------------
-
-    function login(cb) {
-        $.ajax({
-            method: 'GET',
-            url: '/teacherRoutes',
-        }).then(result => {
-            cb(result);
-        });
-    }
-
-    function createAccount(fullName, email, password, cb) {
-        $.ajax({
-            method: 'POST',
-            url: '/teacherRoutes',
-            data: {
-                name: fullName,
-                email: email,
-                password: password,
-                picture: null
-            }
-        }).then(res => {
-            cb(res);
-        });
-    };
-
 
 });
