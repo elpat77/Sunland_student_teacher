3d96ab9c208d8f6a263ec4dffa6523665959dea9
elpat7 <aguilarpat7@gmail.com>
2020-01-22 18:02:18 -0800

    adding searchClassBtn on admin  search

--- views/assets/javascript/calendar.js
+++ views/assets/javascript/calendar.js
@@ -1,91 +1,53 @@
 $(document).ready(function () {
-    document.addEventListener('DOMContentLoaded', function () {
-        var Calendar = FullCalendar.Calendar;
-        var Draggable = FullCalendarInteraction.Draggable
 
-        /* initialize the external events
-        -----------------------------------------------------------------*/
-
-        var containerEl = document.getElementById('external-events-list');
-        var eventEls = Array.prototype.slice.call(
-            containerEl.querySelectorAll('.fc-event')
-        );
-        eventEls.forEach(function (eventEl) {
-            new Draggable(eventEl, {
-                eventData: {
-                    title: eventEl.innerText.trim(),
-                },
-            });
-        });
-
-        /* initialize the calendar
-        -----------------------------------------------------------------*/
-
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: ['bootstrap', 'interaction', 'dayGrid', 'timeGrid', 'list'],
-            themeSystem: 'bootstrap',
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
-
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        defaultDate: '2014-06-12',
+        defaultView: 'month',
+        editable: true,
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2014-06-01'
             },
-            footer: {
-                center: 'addEventButton',
+            {
+                title: 'Long Event',
+                start: '2014-06-07',
+                end: '2014-06-10'
             },
-            editable: true,
-            droppable: true, // this allows things to be dropped onto the calendar
-            drop: function (arg) {
-                // is the "remove after drop" checkbox checked?
-                if (document.getElementById('drop-remove').checked) {
-                    // if so, remove the element from the "Draggable Events" list
-                    arg.draggedEl.parentNode.removeChild(arg.draggedEl);
-                }
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2014-06-09T16:00:00'
             },
-            eventClick: function (arg) {
-                if (confirm('delete event?')) {
-                    arg.event.remove()
-                }
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2014-06-16T16:00:00'
             },
-
-            // eventClick: function (calEvent, jsEvent) {
-            //     var title = prompt('Event Title:', calEvent.title, {
-            //         buttons: {
-            //             Ok: true,
-            //             Cancel: false
-            //         }
-            //     });
-
-            //     if (title) {
-            //         calEvent.title = title;
-            //         ('#calendar').fullCalendar('updateEvent', calEvent);
-            //     }
-            // },
-
-            customButtons: {
-                addEventButton: {
-                    text: 'Add New Event',
-                    click: function () {
-                        var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-                        var date = new Date(dateStr + 'T00:00:00'); // will be in local time
-
-                        if (!isNaN(date.valueOf())) { // valid?
-                            calendar.addEvent({
-                                title: "new event",
-                                start: date,
-                                allDay: true,
-                                editable: true
-                            });
-                            alert('Event added. Now, update your database...');
-                        } else {
-                            alert('Invalid date.');
-                        }
-                    }
-                }
+            {
+                title: 'Meeting',
+                start: '2014-06-12T10:30:00',
+                end: '2014-06-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2014-06-12T12:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2014-06-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2014-06-28'
             }
-        });
-        calendar.render();
-
+        ]
     });
+
 });
